7ba4ad2671b9efad03bd9e08ba12458a3e790569
Bentleyj <jgbentley10@gmail.com>
2017-05-19 17:45:48 +0100

    Added event listener to the project and subscribed to the VRInteractiveItem event OnOver

---
+++ Assets/Scripts/PerspectiveSwitcher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRStandardAssets.Utils;
+
+public class PerspectiveSwitcher : MonoBehaviour {
+
+    public VRInteractiveItem item;
+
+	// Use this for initialization
+	void Start () {
+        item.OnOver += switchPerspective;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    void switchPerspective()
+    {
+        Debug.Log("I've been called!");
+    }
+}
