a0ac1abd74369a7f7ce51a0109a32c9d3e1ab6bd
Bentleyj <jgbentley10@gmail.com>
2017-05-23 15:01:36 +0100

    More comments. I'm the comment king!

--- Assets/Scripts/PlayerMount.cs
+++ Assets/Scripts/PlayerMount.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using VRStandardAssets.Utils;
 
+
+// Written by James Bentley 21th May 2017
+// Really simple class that wraps some fields that I was easy access to. This cannot extend VRInteractiveItem because
+// We want the item to be attached to the avatar not to the mount and we want this to be attached to the mount
 public class PlayerMount : MonoBehaviour {
 
     public GameObject avatar;
@@ -11,10 +15,9 @@ public class PlayerMount : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        // Find the VRInteractiveItem in the children
         if(avatar.GetComponent<VRInteractiveItem>() == null)
             interactiveItem = avatar.AddComponent<VRInteractiveItem>();
-        //t = new Transform(transform);
-        //t.position = new Vector3(t.position.x, t.position.y + 5, t.position.z);
 	}
 	
 	// Update is called once per frame
